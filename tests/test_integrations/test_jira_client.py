b9b649df9b2fa14f6ebabb915a7dabae5474cf2d
frostcoded <frostapple@protonmail.com>
2025-06-06 14:22:22 -0400

    Initial code

--- tests/test_integrations/test_jira_client.py
+++ tests/test_integrations/test_jira_client.py
@@ -1,1 +1,6 @@
+import pytest
+from integrations.jira_client import JiraClient
 
+def test_jira_client_stub():
+    client = JiraClient(config={"url": "https://fake.atlassian.net"})
+    assert hasattr(client, "create_issue")
