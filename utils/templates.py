c57a3243077db33475c2bfca767b562e5f7d04bb
frostcoded <frostapple@protonmail.com>
2025-06-06 14:04:32 -0400

    Initial code

---
+++ utils/templates.py
@@ -0,0 +1,18 @@
+# utils/templates.py
+
+BASE_RELEASE_NOTE_TEMPLATE = """
+# Release Notes - {version}
+
+## Summary
+{summary}
+
+## Fixed Issues
+{issues}
+
+## Commits
+{commits}
+
+---
+
+*Generated on {date}*
+"""
