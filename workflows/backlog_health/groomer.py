6bbdf94ef13e16fc5a1f0fdb9f86e0ae33559ac4
frostcoded <frostapple@protonmail.com>
2025-06-06 14:07:48 -0400

    Initial code

--- workflows/backlog_health/groomer.py
+++ workflows/backlog_health/groomer.py
@@ -1,1 +1,8 @@
+# Automates grooming using the backlog_groomer agent
+from agents.backlog_groomer import BacklogGroomer
+from config.settings import load_settings
 
+def groom_backlog(jira_issues):
+    config = load_settings()
+    groomer = BacklogGroomer(config)
+    return groomer.groom_issues(jira_issues)
