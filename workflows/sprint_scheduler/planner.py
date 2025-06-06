bded52edff2ceabb59c80913e914911ab063d33b
frostcoded <frostapple@protonmail.com>
2025-06-06 14:08:56 -0400

    Initial code

--- workflows/sprint_scheduler/planner.py
+++ workflows/sprint_scheduler/planner.py
@@ -1,1 +1,7 @@
+# Uses sprint_planner agent to organize upcoming sprints
+from agents.sprint_planner import SprintPlanner
+from config.settings import load_settings
 
+def create_sprint_plan(duration, availability, velocity, goals):
+    planner = SprintPlanner(load_settings())
+    return planner.plan_sprint(duration, availability, velocity, goals)
