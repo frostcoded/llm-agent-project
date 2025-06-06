fd94d5cf0cd779f9b2b7cb675aed997a3736b85f
frostcoded <frostapple@protonmail.com>
2025-06-06 14:23:59 -0400

    Initial code

--- tests/test_utils/test_helpers.py
+++ tests/test_utils/test_helpers.py
@@ -1,1 +1,10 @@
+from datetime import datetime
+from utils.helpers import format_datetime, safe_get
 
+def test_format_datetime():
+    now = datetime.utcnow()
+    assert "T" in format_datetime(now)
+
+def test_safe_get():
+    data = {"a": {"b": {"c": "value"}}}
+    assert safe_get(data, ["a", "b", "c"]) == "value"
