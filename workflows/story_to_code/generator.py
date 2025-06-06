36b111c542636db15d3515079fabff2f5443483a
frostcoded <frostapple@protonmail.com>
2025-06-06 14:09:38 -0400

    Initial code

--- workflows/story_to_code/generator.py
+++ workflows/story_to_code/generator.py
@@ -1,1 +1,7 @@
+# Converts Jira stories to code using code_generator
+from agents.code_generator import CodeGenerator
+from config.settings import load_settings
 
+def generate_code_from_story(story_description):
+    generator = CodeGenerator(load_settings())
+    return generator.generate_code(story_description)
